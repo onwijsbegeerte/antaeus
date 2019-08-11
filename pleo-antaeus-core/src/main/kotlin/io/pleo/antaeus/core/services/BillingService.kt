feb028e789bed341e7c7210ff44f006400cf1d4c
mendel <mendel.agterberg@gmail.com>
2019-08-11 20:23:46 +0200

    add first version of the billing service

--- pleo-antaeus-core/src/main/kotlin/io/pleo/antaeus/core/services/BillingService.kt
+++ pleo-antaeus-core/src/main/kotlin/io/pleo/antaeus/core/services/BillingService.kt
@@ -1,9 +1,49 @@
 package io.pleo.antaeus.core.services
 
 import io.pleo.antaeus.core.external.PaymentProvider
+import io.pleo.antaeus.models.Invoice
+import io.pleo.antaeus.models.InvoiceStatus
+import io.pleo.antaeus.models.TransactionStatus
 
 class BillingService(
-    private val paymentProvider: PaymentProvider
+    private val paymentProvider: PaymentProvider,
+    private val invoiceService: InvoiceService
 ) {
-   // TODO - Add code e.g. here
+
+    fun chargeAllInvoices() {
+        val unpaidInvoices =
+                invoiceService
+                        .fetchAll()
+                        .filter { it -> it.status == InvoiceStatus.PENDING }
+
+        charge(unpaidInvoices)
+    }
+
+    private fun charge(unpaidInvoices : List<Invoice>) {
+        unpaidInvoices
+                .forEach { it -> chargeAllInvoices(it, paymentProvider::charge, invoiceService::payInvoice)}
+
+        val stillUnpaidInvoices = invoiceService.fetchAll().filter { it -> it.status == InvoiceStatus.PENDING }
+        if (stillUnpaidInvoices.any()){
+
+            charge(stillUnpaidInvoices)
+        }
+    }
+
+    private fun chargeAllInvoices(invoice: Invoice, tryPayInvoice: (Invoice) -> Boolean, updateInvoice: (Invoice) -> Invoice) {
+
+        invoiceService.createInvoiceChargeStatus(invoice, TransactionStatus.Attempted)
+
+        if(tryPayInvoice(invoice)){
+
+            invoiceService.createInvoiceChargeStatus(invoice, TransactionStatus.Success)
+            updateInvoice(invoice)
+        }
+        else{
+
+            invoiceService.createInvoiceChargeStatus(invoice, TransactionStatus.Failed)
+        }
+    }
+
 }
+
