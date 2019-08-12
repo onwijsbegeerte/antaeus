aca88abd6e66d4ecec98919d0a3ec4cc3286550b
mendel <mendel.agterberg@gmail.com>
2019-08-12 18:58:25 +0200

    add test to make sure payment is attempted when there are unpaid invoices

--- pleo-antaeus-core/src/test/kotlin/io/pleo/antaeus/core/services/BillingServiceTest.kt
+++ pleo-antaeus-core/src/test/kotlin/io/pleo/antaeus/core/services/BillingServiceTest.kt
@@ -18,7 +18,6 @@ class BillingServiceTest {
     fun `Attempting to charging customers, with no unpaid invoices, should not charge customers`() {
 
         val dal = mockk<AntaeusDal> {
-            every { fetchInvoice(404) } returns null
             every { fetchInvoices() } returns listOf(Invoice(1, 2, Money(10.toBigDecimal(), Currency.DKK), InvoiceStatus.PAID ))
             every { createInvoiceChargeStatus(any<Int>(), any(), any<Int>() )} returns InvoiceChargeStatus(Random.nextInt(), Random.nextInt(), Random.nextInt(), TransactionStatus.Attempted, LocalDate.now().toString())
             every { payInvoice(any())} returns Invoice(1, 2, Money(10.toBigDecimal(), Currency.DKK), InvoiceStatus.PAID )
@@ -33,6 +32,27 @@ class BillingServiceTest {
         billingService.chargeAllInvoices()
 
         verify(exactly = 0) { mockPaymentProvider.charge(any()) }
+        confirmVerified(mockPaymentProvider)
+    }
+
+    @Test
+    fun `Attempting to charging customers, some unpaid invoices, should attempt to charge customer`() {
+
+        val dal = mockk<AntaeusDal> {
+            every { fetchInvoices() } returns listOf(Invoice(1, 2, Money(10.toBigDecimal(), Currency.DKK), InvoiceStatus.PENDING )) andThen listOf()
+            every { createInvoiceChargeStatus(any<Int>(), any(), any<Int>() )} returns InvoiceChargeStatus(Random.nextInt(), Random.nextInt(), Random.nextInt(), TransactionStatus.Attempted, LocalDate.now().toString())
+            every { payInvoice(any())} returns Invoice(1, 2, Money(10.toBigDecimal(), Currency.DKK), InvoiceStatus.PAID )
+        }
+
+        val mockPaymentProvider = mockk<PaymentProvider>{
+            every { charge(any()) } returns true
+        }
+
+        val billingService = BillingService(mockPaymentProvider, InvoiceService(dal))
+
+        billingService.chargeAllInvoices()
+
+        verify(exactly = 1) { mockPaymentProvider.charge(any()) }
         confirmVerified(mockPaymentProvider)
     }
 }
